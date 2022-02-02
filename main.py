59bd4b8922786c772eb7d1f280fe170ce244386f
Nikolas Achatz <75917813+nachatz@users.noreply.github.com>
2022-02-01 18:53:48 -0800

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,37 @@
 
+from load_data import loadData
+from utils import imshow
+import torch
+import torchvision
+import torchvision.transforms as transforms
+import matplotlib.pyplot as plt
+import numpy as np
+import torch.optim as optim
+import torch.nn as nn
+import torch.nn.functional as F
+from simplecnn import Net
+from train import train
+
+
+
+def main():
+    net = Net()
+    classes, testloader, trainloader = loadData()
+
+    # get some random training images
+    dataiter = iter(trainloader)
+    images, labels = dataiter.next()
+
+    # show images
+    imshow(torchvision.utils.make_grid(images))
+    # print labels
+    print(' '.join(f'{classes[labels[j]]:5s}' for j in range(4)))
+
+    
+    criterion = nn.CrossEntropyLoss()
+    optimizer = optim.SGD(net.parameters(), lr=0.001, momentum=0.9)
+
+    train(trainloader, net, optimizer, criterion)
+
+if __name__ == "__main__":
+    main()
