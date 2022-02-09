7a3c4f75dd40b6186d71ede10a46a63e700d16b1
Nathan Rumsey <nathan.s.rumsey@gmail.com>
2022-02-09 01:35:44 -0800

    Fix download link

--- download_dataset.sh
+++ download_dataset.sh
@@ -9,7 +9,7 @@ else
     cd data/
 fi
 
-wget https://drive.google.com/u/1/uc?id=1HUpMP3zu8dwrFYyPObVwuAob-oJHh57K&export=download -O CrowdHumanDataset.zip
+wget -L https://oregonstate.box.com/shared/static/u4gnvq0cyksjz242ywuo66k9cechmyan -O CrowdHumanDataset.zip
 
 echo ""
 echo "Download Complete!"
