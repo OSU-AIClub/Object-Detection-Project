00c0e9e76e79d01ff311ae2b0c6db3784b778cf7
Nikolas Achatz <75917813+nachatz@users.noreply.github.com>
2022-03-01 17:54:01 -0800

    Import prebuilt model and evaluate w/ OpenCV
    Prebuilt model trained on COCO dataset

--- main.py
+++ main.py
@@ -1,1 +1,21 @@
+import torch
+from matplotlib import pyplot as plt
+import numpy as np
+import cv2
 
+model = torch.hub.load('ultralytics/yolov5', 'yolov5s')
+
+
+cap = cv2.VideoCapture(0)
+while cap.isOpened():
+    ret, frame = cap.read()
+    
+    # Make detections 
+    results = model(frame)
+    
+    cv2.imshow('YOLO', np.squeeze(results.render()))
+    
+    if cv2.waitKey(10) & 0xFF == ord('q'):
+        break
+cap.release()
+cv2.destroyAllWindows()
